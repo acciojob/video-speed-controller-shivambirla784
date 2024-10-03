11e7ed0e913b1f8edf5f8d60a07e3b1f5eb80ff6
shivambirla784 <86475649+shivambirla784@users.noreply.github.com>
2024-10-03 21:34:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Select elements
 const player = document.querySelector('.player');
-const video = player.querySelector('.viewer');
+const video = player.querySelector('.player__video'); // Updated class
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
